d9534dab4e5b3230cf8156fa629a732da9dc248e
HairyOtter07 <hairyotter07@gmail.com>
2026-04-13 17:51:13 -0700

    implement start_gradescope_session

---
+++ functions.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE = "https://www.gradescope.com"
+
+
+def _endpoint(e):
+    return BASE + e
+
+
+def start_gradescope_session(email: str, password: str) -> requests.Session:
+    s = requests.Session()
+    s.headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:145.0) Gecko/20100101 Firefox/145.0"
+        }
+    )
+
+    r = s.get(_endpoint("/login"))
+    loginPage = BeautifulSoup(r.content, features="html.parser")
+    authForm = loginPage.find("form")
+    if authForm is None:
+        return s
+    authTokenInput = authForm.find("input", attrs={"name": "authenticity_token"})
+    if authTokenInput is None:
+        return s
+    authToken = str(authTokenInput["value"])
+    loginData = {
+        "authenticity_token": authToken,
+        "session[email]": email,
+        "session[password]": password,
+    }
+    s.post(_endpoint("/login"), data=loginData)
+    return s
