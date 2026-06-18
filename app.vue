68c023541f1a95c059c625bc238eef6c03a358dc
HairyOtter07 <hairyotter07@gmail.com>
2026-06-17 21:20:34 -0700

    add font

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
 <template>
-    <NuxtPage />
+    <NuxtPage class="w-full h-full font-[Ubuntu]" />
 </template>
+<script setup>
+useHead({
+    link: [
+        {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        },
+        {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+        },
+        {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap",
+        },
+    ],
+});
+</script>
