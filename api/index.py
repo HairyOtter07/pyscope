5f04e7dae8c9c71bfb808b1c6a3f939869462558
HairyOtter07 <hairyotter07@gmail.com>
2026-06-28 18:56:21 -0700

    update hosts

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ GS_DATETIME_FSTRING = "%Y-%m-%d %H:%M:%S %z"
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
-origins = ["http://localhost:3000", "https://betterscope-frontend.vercel.app"]
+origins = ["http://localhost:3000", "https://betterscope.hairyotter07.dev"]
 
 app.add_middleware(
     CORSMiddleware,
